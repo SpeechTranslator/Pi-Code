9580e31587eed09082d08931e9127c4951f3a807
simonguozirui <simonguozirui@gmail.com>
2017-01-07 22:04:46 -0500

    change duration, active mode

--- stt.sh
+++ stt.sh
@@ -1,5 +1,5 @@
 echo "Recording your Speech (Ctrl+C to Transcribe)"
-arecord -D plughw:0,0 -q -f cd -t wav -d 0 -r 16000 | flac - -f --best --sample-rate 16000 -s -o daveconroy.flac;
+arecord -D plughw:1,0 -q -f cd -t wav -d 1 -r 16000 | flac - -f --best --sample-rate 16000 -s -o daveconroy.flac;
 
 echo "Converting Speech to Text..."
 wget -q -U "Mozilla/5.0" --post-file daveconroy.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium" | cut -d\" -f12  > stt.txt
