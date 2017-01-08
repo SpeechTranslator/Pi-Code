cf8af1822bc2f6872705a9b90a391bed1afb2753
simonguozirui <simonguozirui@gmail.com>
2017-01-07 22:19:49 -0500

    update

--- stt.sh
+++ stt.sh
@@ -2,7 +2,7 @@ echo "Recording your Speech (Ctrl+C to Transcribe)"
 arecord -D plughw:1,0 -q -f cd -t wav -d 1 -r 16000 | flac - -f --best --sample-rate 16000 -s -o daveconroy.flac;
 
 echo "Converting Speech to Text..."
-wget -q -U "Mozilla/5.0" --post-file daveconroy.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium" | cut -d\" -f12  > stt.txt
+wget -q -U "Mozilla/5.0" --post-file daveconroy.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium&key=AIzaSyA87UP0tfwjHg1ZeQUuHkCNwNF-vxi5gaY" | cut -d\" -f12  > stt.txt
 
 echo "You Said:"
 value=`cat stt.txt`
