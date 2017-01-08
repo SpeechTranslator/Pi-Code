0d9e2ba2b459b346f531bcda733681c342ed16ef
simonguozirui <simonguozirui@gmail.com>
2017-01-08 00:23:29 -0500

    new code

--- stt.sh
+++ stt.sh
@@ -1,9 +1,5 @@
-echo "Recording your Speech (Ctrl+C to Transcribe)"
-arecord -D plughw:1,0 -q -f cd -t wav -d 5 -r 16000 | flac - -f --best --sample-rate 16000 -s -o daveconroy.flac;
-
-echo "Converting Speech to Text..."
-wget -q -U "Mozilla/5.0" --post-file daveconroy.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium&key=AIzaSyAzKBBOYHbWIrKVy2cvri2uQKwF1on9Nwc" | cut -d\" -f12  > stt.txt
-
-echo "You Said:"
-value=`cat stt.txt`
-echo "$value"
+arecord -D plughw:2,0 -f cd -t wav -d 10 -r 16000 | \
+flac - -f --best --sample-rate 16000 -o out.flac; \
+wget -O - -o /dev/null --post-file out.flac --header="Content-Type: audio/x-flac; rate=16000" \
+"http://www.google.com/speech-api/v2/recognize?lang=en-us&key=AIzaSyAzKBBOYHbWIrKVy2cvri2uQKwF1on9Nwc&output=json" | \
+sed -e 's/[{}]/''/g'
